a3cc3322b2d7961f087f67dea6959a7b2ff48a24
CharlieHii <charlie_chaper@hotmail.com>
2024-06-25 14:41:48 +0700

    feat: create get all api

--- server/app.mjs
+++ server/app.mjs
@@ -1,10 +1,27 @@
 import express from "express";
+import connectionPool from "./utils/db.mjs";
 
 const app = express();
 const port = 4001;
 
+app.use(express.json());
+
 app.get("/test", (req, res) => {
   return res.json("Server API is working 🚀");
+});
+
+app.get("/assignments", async (req, res) => {
+  let result;
+  try {
+    result = await connectionPool.query("select * from assignments");
+  } catch {
+    return res.status(500).json({
+      message: "Server could not read assignment because database connection",
+    });
+  }
+  return res.status(200).json({
+    data: result.rows[0],
+  });
 });
 
 app.listen(port, () => {
